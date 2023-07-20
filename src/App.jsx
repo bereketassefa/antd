c1cef0dfada821a3a4bc69fb6d1dd0751cbd7d2f
Dano Hailu <danohailu11@gmail.com>
2023-07-20 12:53:13 +0300

    new build test

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import Primary from './Layouts/Primary/primary'
 import Home from './Pages/Home/home'
-import Message from './Pages/Message/message'
+// import Message from './Pages/Message/message'
 import Notification from './Pages/Notification/notification'
 
 function App() {
@@ -16,7 +16,7 @@ function App() {
             <Route index element={<Home />} />
 
             <Route path='/notifications' element={<Notification />}  />
-            <Route path='/messages' element={<Message />} />
+            {/* <Route path='/messages' element={<Message />} /> */}
 
             
         </Route>
