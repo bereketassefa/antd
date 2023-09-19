dc662ac4d7e6bf8405673a81569b4244e93f3432
Hawera <hawibekana2017@gmail.com>
2023-09-19 10:29:37 +0300

    profile change and delete

--- src/Components/Profile/ProfileFeature/CompanyInfo/CompanyPP.jsx
+++ src/Components/Profile/ProfileFeature/CompanyInfo/CompanyPP.jsx
@@ -1,27 +1,120 @@
-import React from "react";
+// import React, { useState } from "react";
+// import profile from "../../../../assets/image/cute-girl-pic (12).jpg";
+// import { RiDeleteBin6Line } from "react-icons/ri";
+// import { MdOutlineFileUpload } from "react-icons/md";
+// function CompanyPP({ profilePic }) {
+//   const [image, setImage] = useState(profile);
+//   const [isEditing, setIsEditing] = useState(false);
+
+//   const handleImageUpload = (event) => {
+//     const newImage = event.target.files[0];
+//     if (newImage) {
+//       const reader = new FileReader();
+//       reader.onload = () => {
+//         setImage(reader.result);
+//       };
+//       reader.readAsDataURL(newImage);
+//     }
+//   };
+
+//   const handleImageDelete = () => {
+//     setImage(profile);
+//   };
+
+//   return (
+//     <div>
+//       <div className="flex justify-between">
+//         <p>Updating profile</p>
+//       </div>
+//       <hr className="border-2 my-2" />
+//       <div className="flex flex-col justify-center items-center">
+//         <p>Help others recognize you!</p>
+//         <div>
+//           <img src={profilePic} alt="" className="w-40 h-40 rounded-full" />
+//         </div>
+//       </div>
+//       <hr className="border-2 my-2 " />
+//       <div className="flex justify-end gap-6">
+//         <label className="bg-gray-200 px-6 py-2 gap-1 rounded-lg flex items-center">
+//           <input
+//             type="file"
+//             accept="image/*"
+//             style={{ display: "none" }}
+//             onChange={handleImageUpload}
+//           />
+//           <MdOutlineFileUpload className="text-xl" />
+//           Update
+//         </label>
+//         <button
+//           className="bg-gray-200 px-6 rounded-lg gap-1 flex items-center"
+//           onClick={handleImageDelete}
+//         >
+//           <RiDeleteBin6Line className="text-[15px]" />
+//           Delete
+//         </button>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default CompanyPP;
+
+
+import React, { useState } from "react";
 import profile from "../../../../assets/image/cute-girl-pic (12).jpg";
-// import { IoClose } from "react-icons/io";
-function CompanyPP() {
+import { RiDeleteBin6Line } from "react-icons/ri";
+import { MdOutlineFileUpload } from "react-icons/md";
+
+function CompanyPP({ profilePic }) {
+  const [image, setImage] = useState(profile);
+  const [isEditing, setIsEditing] = useState(false);
+
+  const handleImageUpload = (event) => {
+    const newImage = event.target.files[0];
+    if (newImage) {
+      const reader = new FileReader();
+      reader.onload = () => {
+        setImage(reader.result);
+      };
+      reader.readAsDataURL(newImage);
+    }
+  };
+
+  const handleImageDelete = () => {
+    setImage(profile);
+  };
+
   return (
     <div>
       <div className="flex justify-between">
-        
-        <p>Upploading profile</p>
+        <p>Updating profile</p>
+      </div>
+      <hr className="border-2 my-2" />
+      <div className="flex flex-col justify-center items-center">
+        <p>Help others recognize you!</p>
         <div>
-          {/* <IoClose /> */}
+          <img src={profilePic} alt="" className="w-40 h-40 object-cover rounded-full" />
         </div>
       </div>
-      <hr />
-      <div>
-        <p>help to recogeize </p>
-        <div>
-          <img src={profile} alt="" className=" w-48 h-48 rounded-full" />
-        </div>
-      </div>
-      <hr />
-      <div className="flex justify-end">
-        <button>update</button>
-        <button>Delete</button>
+      <hr className="border-2 my-2 " />
+      <div className="flex justify-end gap-6">
+        <label className="bg-gray-200 px-6 py-2 gap-1 rounded-lg flex items-center">
+          <input
+            type="file"
+            accept="image/*"
+            style={{ display: "none" }}
+            onChange={handleImageUpload}
+          />
+          <MdOutlineFileUpload className="text-xl" />
+          Update
+        </label>
+        <button
+          className="bg-gray-200 px-6 rounded-lg gap-1 flex items-center"
+          onClick={handleImageDelete}
+        >
+          <RiDeleteBin6Line className="text-[15px]" />
+          Delete
+        </button>
       </div>
     </div>
   );
