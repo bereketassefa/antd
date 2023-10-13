658cf43933857d020c34c65b0642e48a533ebd6c
fitsum <fitsumgetu88@gmail.com>
2023-10-13 16:54:11 +0300

    new

--- src/Components/Profile/ProfileFeature/FeatureImgHolder/featureImgHolder.jsx
+++ src/Components/Profile/ProfileFeature/FeatureImgHolder/featureImgHolder.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import ProfileFeature from "../../../../assets/img/ProfileFeature1.png";
+import ProfileFeature from "../../../../assets/img/ProfileFeature.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEdit } from "@fortawesome/free-solid-svg-icons";
 import { useCookies } from "react-cookie";
