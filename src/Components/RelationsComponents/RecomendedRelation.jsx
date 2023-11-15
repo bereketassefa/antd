167dfe150fa97b029ac74ac450afcc458642d555
Fitsum Getu <fitsumgetu88@gmail.com>
2023-11-15 17:47:17 +0300

    nav bar , recommendations

--- src/Components/RelationsComponents/RecomendedRelation.jsx
+++ src/Components/RelationsComponents/RecomendedRelation.jsx
@@ -1,54 +1,103 @@
-import React, { useState } from "react";
-import { BsFillPersonPlusFill } from "react-icons/bs";
-import Button from "../../Fields/Button/button";
+import React, { useEffect, useState } from "react";
+import { faUserPlus, faCheck } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Avatar from "../../Fields/Avatar/avatar";
-import { Recomend } from "../../data";
 import axios from "axios";
+import alternativeProfile from "../../assets/image/alternativeProfile.png";
 import { useCookies } from "react-cookie";
+
 function RecomendedRelation() {
   const [DataRecommendation, setRecommendation] = useState([]);
+  const [loadingSend, setLoadingSend] = useState(false);  // Add this line
   const [cookies] = useCookies(['User']);
+
+  const ownerUid = cookies?.user?.Uid;
 
   const fetchRecomendation = () => {
     axios
-      .post(import.meta.env.VITE_GET_RECOMMENDATION,{ Uid: cookies?.user.Uid })
+      .post(import.meta.env.VITE_GET_RECOMMENDATION, { Uid: cookies?.user?.Uid })
       .then((res) => {
-        console.log("Recommendation response:", res.data); // Log the response
-        // Check if the data is an array, if not, set it to an empty array
         const recommendationData = Array.isArray(res.data) ? res.data : [];
-        setRecommendation(recommendationData);
+        // Add loading and success states to each recommendation
+        const recommendationWithStates = recommendationData.map((recomeded) => ({
+          ...recomeded,
+          loadingSend: false,
+          isSuccessful: false,
+        }));
+        setRecommendation(recommendationWithStates);
       });
   };
-  // fetchRecomendation();
-  return (
-    <div className="mx-auto grid  grid-cols-2 justify-items-center gap-5 px-2 sm:grid-cols-3 sm:px-0  pb-4 lg:grid-cols-4">
-      {DataRecommendation.map((recomeded) => {
-        return (
-          <div className="flex-col  justify-center items-center w-[185px] h-[160px] border-2 border-blue-800 ">
-            <div className=" flex-col items-center justify-center py-4 ">
-              <div className=" flex justify-center">
-                {" "}
-                <Avatar img={recomeded?.account?.party} />
-              </div>
-              <h2 className=" mt-2 font-bold text-[#000] text-center text-[17px]">
-                {recomeded.title}
-              </h2>
-            </div>
 
+  useEffect(() => {
+    fetchRecomendation();
+  }, []);
+
+  const handleRequestConectionlClick = async (recomededIndex) => {
+    setLoadingSend(true);
+    try {
+      const url = `${import.meta.env.VITE_SEND_CONNECION}`;
+      const res = await axios.post(
+        url,
+        {
+          node1: ownerUid.toString(),
+          node2: DataRecommendation[recomededIndex]?.account?.Uid?.toString(),
+        },
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+
+      // Update the success state for the specific recommendation card
+      setRecommendation((prevRecommendations) => {
+        const updatedRecommendations = [...prevRecommendations];
+        updatedRecommendations[recomededIndex].isSuccessful = true;
+        return updatedRecommendations;
+      });
+      fetchRecomendation()
+    } catch (error) {
+      console.error(
+        "Error in handleRequestConnectionClick:",
+        error.response ? error.response.data : error.message
+      );
+      fetchRecomendation()
+    } finally {
+      // Update the loading state for the specific recommendation card
+      setRecommendation((prevRecommendations) => {
+        const updatedRecommendations = [...prevRecommendations];
+        updatedRecommendations[recomededIndex].loadingSend = false;
+        return updatedRecommendations;
+      });
+      setLoadingSend(false);
+      fetchRecomendation();
+    }
+  };
+
+  return (
+    <div className="mx-auto grid grid-cols-2 justify-items-center gap-2 px-2 sm:grid-cols-3 sm:px-0 pb-4 lg:grid-cols-3">
+      {DataRecommendation.map((recomeded, index) => (
+        <div key={`${recomeded?.account?.Uid}_${index}`} className="flex-col justify-center items-center w-[185px] h-[160px] border-2 border-blue-800">
+          <div className="flex-col items-center justify-center py-4">
             <div className="flex justify-center">
-              {" "}
-              <Button
-                text="Accept"
-                icon={<BsFillPersonPlusFill />}
-                iconPossition="left"
-                color="[#D71A62]"
-                filled
-                onClick={null}
-              />
+              <Avatar img={recomeded?.account?.profilePicture ? recomeded?.account?.profilePicture : alternativeProfile} />
             </div>
+            <h2 className="mt-2 text-[#000] text-center text-[17px]">
+              {recomeded?.account?.party}
+            </h2>
           </div>
-        );
-      })}
+
+          <div className="flex justify-center">
+            {recomeded.loadingSend ? (
+              <div className="spinner"></div>
+            ) : recomeded.isSuccessful ? (
+              <FontAwesomeIcon icon={faCheck} className='text-largeP md:text-smallT text-success' />
+            ) : (
+              <FontAwesomeIcon onClick={() => handleRequestConectionlClick(index)} icon={faUserPlus} className='text-largeP md:text-smallT text-secondary cursor-pointer' />
+            )}
+          </div>
+        </div>
+      ))}
     </div>
   );
 }
--- src/Components/RelationsComponents/RequestedRelation.jsx
+++ src/Components/RelationsComponents/RequestedRelation.jsx
@@ -1,15 +1,21 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Requested } from "../../data";
 import Button from "../../Fields/Button/button";
 import Avatar from "../../Fields/Avatar/avatar";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCheck, faClose } from '@fortawesome/free-solid-svg-icons'
 import axios from "axios";
 import { useCookies } from "react-cookie";
+import alternativeProfile from "../../assets/image/alternativeProfile.png";
 function RequestedRelation() {
   
   const [dataRelation, setDataRelation] =useState([])
+  const [loadingAccept, setLoadingAccept] = useState(false);
+  const [loadingCancel, setLoadingCancel] = useState(false);
   const [cookies] = useCookies(['User']);
 
     const handleAcceptClick = async () => {
+      setLoadingAccept(true);
     try {
       const url = `${import.meta.env.VITE_ACCEPT_THE_RELATION}/${dataRelation[0]?.connections?.id}`;
       const response = await fetch(url, {
@@ -21,9 +27,12 @@ function RequestedRelation() {
       }
 
       const data = await response.json();
+      fetchRelationRequest()
       //   console.log(data); // Process the response data if needed
     } catch (error) {
-      console.error(error); // Handle the error, e.g., show an error message
+      console.error(error);
+    } finally {
+      setLoadingAccept(false);
     }
   };
   const fetchRelationRequest = () => {
@@ -38,33 +47,66 @@ function RequestedRelation() {
          
       });
   };
-  fetchRelationRequest()
+  useEffect(()=>{
+ fetchRelationRequest()
+  })
+  const handleCancelClick = async () => {
+    setLoadingCancel(true);
+    try {
+      
+      // Artificially delay the operation by 6 seconds
+     
+      // 
+      const url = `${import.meta.env.VITE_CANCEL_THE_RELATION}/${dataRelation[0]?.connections?.id}`;
+      const response = await fetch(url, { method: "DELETE" });
+      if (!response.ok) throw new Error("Request failed");
+      const data = await response.json();
+      // console.log(data)
+      await new Promise(resolve => setTimeout(resolve, 9000));
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoadingCancel(false);
+    }
+  };
+  
+
+  const hadleNavigateProfile = async(e)=>{
+    e.preventDefault();
+    // navigate(`/feed/profile/${Uid}`)
+}
+
+
   return (
     <div className="flex flex-col  gap-4 mx-auto pb-4">
       {dataRelation.map((item) => {
-  const partyName = item?.account[0]?.party || '';
+  const partyName = item?.account?.party || '';
   const firstLetter = partyName.charAt(0).toLowerCase();
   
   return (
     <div className="flex flex-col md:flex-row justify-between md:items-center border-[1px] border-[#3222C6]   px-4  ">
       <div className=" justify-start flex items-center lg:justify-center gap-2 ">
-        {item.image ? (
-          <Avatar img={item.image} />
-        ) : (
-          <div className="bg-black w-10 h-10 flex items-center justify-center rounded-full">
-            <span className="text-white">{firstLetter}</span>
-          </div>
-        )}
-        <h2 className=" font-bold text-[#000] text-center text-[17px] ">
+      <Avatar onClick={hadleNavigateProfile}  img={ alternativeProfile } />
+        <h2 className=" text-[#000] text-center text-[17px] ">
           {partyName}
         </h2>
       </div>
 
             <div className="flex gap-4 py-4 items-center">
-              <Button onClick={handleAcceptClick} text={"Accept"} filled color="[#D71A62]" />
-              <Button text={"Decline"} color="[#D71A62]" />
+            <Button
+                onClick={handleAcceptClick}
+                text={loadingAccept ? <div className="spinner"></div> : "Accept"}
+                filled
+                color="[#D71A62]"
+                disabled={loadingAccept}
+              />
+              <Button  onClick={handleCancelClick}
+                text={loadingCancel ? <div className="spinner"></div> : "Decline"}
+                filled
+                color="[#D71A62]"
+                disabled={loadingCancel} />
 
-              <p className="hidden lg:block">{item.time} min</p>
+              {/* <p className="hidden lg:block">{item.time} min</p> */}
             </div>
           </div>
         );
